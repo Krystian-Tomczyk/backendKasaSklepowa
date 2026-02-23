8647653f9d3a168d1a0beb555f14812a574cdf6f
Krystian-Tomczyk <krystian.tomczyk@tm1.edu.pl>
2026-02-23 11:59:36 +0100

    getters,setters

--- src/main/java/org/example/backendkasasklepowa/model/Product.java
+++ src/main/java/org/example/backendkasasklepowa/model/Product.java
@@ -30,4 +30,52 @@ public class Product {
 
     // Ilość na stanie
     private int stockQuantity;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getBarcode() {
+        return barcode;
+    }
+
+    public void setBarcode(String barcode) {
+        this.barcode = barcode;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+
+    public int getStockQuantity() {
+        return stockQuantity;
+    }
+
+    public void setStockQuantity(int stockQuantity) {
+        this.stockQuantity = stockQuantity;
+    }
 }
